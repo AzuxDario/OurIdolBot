2578ea0ae39fcb0c0de19cc94564bcd8c1b73edc
AzuxDario <pawel_mibor95@tlen.pl>
2018-05-17 21:25:23 +0200

    Initial commit

---
+++ OurIdolBot/Core/Bot.cs
@@ -0,0 +1,102 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Net.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurIdolBot.Core
+{
+    class Bot
+    {
+        public struct ConfigJson
+        {
+            [JsonProperty("token")]
+            public string Token { get; private set; }
+
+            [JsonProperty("prefix")]
+            public string CommandPrefix { get; private set; }
+        }
+
+
+        public static DiscordClient DiscordClient { get; set; }
+        private CommandsNextModule _commands { get; set; }
+
+        public void Run()
+        {
+            Connect();
+            SetNetworkParameters();
+        }
+
+        private async void Connect()
+        {
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var cfg = new DiscordConfiguration
+            {
+                Token = cfgjson.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            var connectionConfig = new DiscordConfiguration
+            {
+                Token = cfgjson.CommandPrefix,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            DiscordClient = new DiscordClient(connectionConfig);
+            DiscordClient.SetWebSocketClient<WebSocket4NetClient>();
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefix = cfgjson.CommandPrefix,
+                EnableDms = true,
+                EnableMentionPrefix = true,
+                CaseSensitive = false
+            };
+
+            _commands = DiscordClient.UseCommandsNext(commandsConfig);
+            _commands.CommandExecuted += Commands_CommandExecuted;
+            _commands.CommandErrored += Commands_CommandErrored;
+
+            await DiscordClient.ConnectAsync();
+        }
+
+        private void SetNetworkParameters()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 |
+                                                   SecurityProtocolType.Tls12;
+        }
+
+        private Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "ExampleBot", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+
+            return Task.FromResult(0);
+        }
+
+        private async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "ExampleBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+        }
+
+
+    }
+}
--- OurIdolBot/Program.cs
+++ OurIdolBot/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OurIdolBot.Core;
+using System;
 
 namespace OurIdolBot
 {
@@ -10,6 +7,8 @@ namespace OurIdolBot
     {
         static void Main(string[] args)
         {
+            new Bot().Run();
+            while (Console.ReadLine() != "quit") ;
         }
     }
 }
