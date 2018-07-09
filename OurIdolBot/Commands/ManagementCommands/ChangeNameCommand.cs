9dec818613d5b8a0b5d0f0eec7077c3ff02ee14c
AzuxDario <pawel_mibor95@tlen.pl>
2018-07-09 20:34:00 +0200

    Add command changing name

---
+++ OurIdolBot/Commands/ManagementCommands/ChangeNameCommand.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using OurIdolBot.Attributes;
+using OurIdolBot.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurIdolBot.Commands.ManagementCommands
+{
+    [CommandsGroup("Management")]
+    class ChangeNameCommand
+    {
+        [Command("changeName")]
+        [Description("Change bot descrition.")]
+        public async Task ChangeName(CommandContext ctx, [Description("New name.")] string name = null)
+        {
+            if (ctx.Member.Id == Bot.configJson.Developer)
+            {
+                await Bot.DiscordClient.EditCurrentUserAsync(name);
+            }
+        }
+    }
+}
