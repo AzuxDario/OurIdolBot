ed4dee27b2d4ab60b486aa02fd7131e3752f4a33
AzuxDario <pawel_mibor95@tlen.pl>
2018-11-16 00:30:31 +0100

    Anime NFO fixed again

--- OurIdolBot/Const/MusicConst/RadiosLinksDataConst.cs
+++ OurIdolBot/Const/MusicConst/RadiosLinksDataConst.cs
@@ -5,6 +5,6 @@
         public static readonly string AnisonFm = "http://anison.fm/status.php?widget=true";
         public static readonly string JMusic = "http://player.abovecast.com/streamdata.php?h=agnes.torontocast.com&p=8083&i=&f=v2&c=";
         public static readonly string BlueIvana = "https://www.radionomy.com/en/OnAir/GetCurrentSongPlayer";
-        public static readonly string AnimeNFO = "http://momori.animenfo.com:8000/";
+        public static readonly string AnimeNFO = "http://emaru.animenfo.com:443/";
     }
 }
